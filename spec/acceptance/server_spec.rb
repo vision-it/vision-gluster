72d7fc52f84cf51c2e42a05c9743fb6235376922
Markus Opolka <opolkams@iis.fraunhofer.de>
2020-09-24 14:55:06 +0200

    Fix test by mocking the service file

--- spec/acceptance/server_spec.rb
+++ spec/acceptance/server_spec.rb
@@ -3,6 +3,16 @@ require 'spec_helper_acceptance'
 describe 'vision_gluster::server' do
   context 'with defaults' do
     it 'run idempotently' do
+      setup = <<-FILE
+        # Just so that we dont get an error while starting the service
+        file { '/etc/init.d/glusterd':
+          ensure => file,
+          mode   => '0755',
+        }
+      FILE
+      apply_manifest(setup, accept_all_exit_codes: true, catch_failures: false)
+
+
       pp = <<-FILE
         class { 'vision_gluster::server': }
       FILE
